8e17a08cbcebde1f1acf426a4f23d64ebff50d77
Vipul Sharma <vipul.sharma@neenopal.com>
2026-05-07 09:02:03 +0000

    Managed identity integration

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Updated via CI/CD 🚀"
-
-if __name__ == "__main__":
-    app.run()
+    secret = os.environ.get("DB_PASSWORD")
+    return f"Retrieved Secret: {secret}"
